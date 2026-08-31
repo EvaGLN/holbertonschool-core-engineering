4ebc91328134cde299fa33b39d247b98f29317b1
EvaGLN <maeva.galiano@gmail.com>
2026-08-31 11:56:11 +0200

    Add structured_output.py

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+v = 3
+pi = 3.14159
+boolean = (1 + 1 == 2)
+
+print("Language: Python")
+print(f"Version: {v}")
+print(f"Pi approx: {pi:.2f}")
+print("Computation valid: {}" .format(boolean))
