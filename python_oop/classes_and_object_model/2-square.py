7c98db47703c6295afe4682085119c9b3b9ba0de
EvaGLN <maeva.galiano@gmail.com>
2026-09-07 16:05:01 +0200

    Add 2-square.py

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for a square size validation"""
+
+
+class Square:
+    """Class for a square with a private instance attribute size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
