d0643eac6b68ce850ad79f19d5409e94ff8c7962
EvaGLN <maeva.galiano@gmail.com>
2026-09-07 15:29:12 +0200

    Add 1-square.py

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for a square with size"""
+
+
+class Square:
+    """Class for a square with a private instance attribute size"""
+    def __init__(self, size)
+        self.__size = size
