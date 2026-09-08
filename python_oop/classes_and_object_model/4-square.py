d2ebe31c4cac1810abc9594c824581574643eeca
EvaGLN <maeva.galiano@gmail.com>
2026-09-08 09:48:54 +0200

    Add 4-square.py

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module for the area of a square"""
+
+
+class Square:
+    """Class to return the area of the square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
