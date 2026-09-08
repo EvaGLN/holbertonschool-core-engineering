02f8501b358e42fceebc38ade7ac9f50b32d77fd
EvaGLN <maeva.galiano@gmail.com>
2026-09-08 11:18:29 +0200

    Minor: remove duplication of code

--- python_oop/classes_and_object_model/5-square.py
+++ python_oop/classes_and_object_model/5-square.py
@@ -5,11 +5,7 @@
 class Square:
     """Class to return the area of the square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
