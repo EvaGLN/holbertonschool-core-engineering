2681401c873593a609c2311bbab170dca8dc49c9
EvaGLN <maeva.galiano@gmail.com>
2026-09-08 11:20:28 +0200

    Minor: remove duplication of code

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-"""Module for the area of a square"""
+"""Module to print a square"""
 
 
 class Square:
-    """Class to return the area of the square"""
+    """Class to print a square"""
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
         self.position = position
 
     @property
