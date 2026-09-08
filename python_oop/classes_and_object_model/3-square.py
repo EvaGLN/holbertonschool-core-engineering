3722d1b3daff50b75a50af71df78d6b5c51a424d
EvaGLN <maeva.galiano@gmail.com>
2026-09-08 09:24:17 +0200

    Add 3-square.py

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for the area of a square"""
+
+
+class Square:
+    """Class to return the area of the square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
