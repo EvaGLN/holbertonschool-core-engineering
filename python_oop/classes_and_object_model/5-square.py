815f353a3588e6f1e6effa8c7b0d663df3585752
EvaGLN <maeva.galiano@gmail.com>
2026-09-08 10:00:39 +0200

    Add 5-square.py

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module for the area of a square"""
+
+
+class Square:
+    """Class to return the area of the square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
