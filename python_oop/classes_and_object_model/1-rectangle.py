8088ecd1720a9810897fb654d945cf7131dfd6a3
EvaGLN <maeva.galiano@gmail.com>
2026-09-08 11:14:52 +0200

    Add 1-rectangle.py

---
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module for the area of a rectangle"""
+
+
+class Rectangle:
+    """Class to return the area of the rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
