7b06b51f7481dbcff41c3cd5afd6c7b544c0c4ee
EvaGLN <maeva.galiano@gmail.com>
2026-09-09 09:59:26 +0200

    Add 2-rectangle.py

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Full Rectangle"""
+
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class of a rectangle inheriting BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
