021235033883aa74b26f3af178c8642f7f26de33
EvaGLN <maeva.galiano@gmail.com>
2026-09-21 11:25:03 +0200

    Add write_file.py

---
+++ python_advanced/file_handling/write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write to a file"""
+
+
+def write_file(filename="", text=""):
+    """Write a function that writes a string to a text file (UTF-8)
+    and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
